529386deee4ffaea2d219cb1f8370b10300a818c
pinit <zproject.4.u@gmail.com>
2025-07-18 19:56:09 +0700

    update code

---
+++ src/pages/page-builder-editor copy/[restaurantId].js
@@ -0,0 +1,102 @@
+import { Layout } from "@components/Base/Layout";
+import { Modal } from "@components/Base/Modal";
+import { ContainerEditorMenu } from "@components/Editor/ContainerEditorMenu";
+import { ContainerFreeformBlocks } from "@components/Editor/ContainerFreeformBlocks";
+import { ImportFreeformBlock } from "@components/Editor/ModalForm/ImportFreeformBlock";
+import _ from "lodash";
+import React, { useRef } from "react";
+import { shallowEqual, useDispatch, useSelector } from "react-redux";
+
+import { DndContext, PointerSensor, useSensor, useSensors, useDraggable, closestCenter } from "@dnd-kit/core";
+import { setFreeformBlocks } from "@redux/reducers/editor/freeformBlocks.reducers";
+import { Container } from "@components/Editor/Container";
+import { Text } from "@components/Base/Text";
+import { LOREM_IPSUM } from "statics/LOREM_IPSUM";
+
+export default () => {
+  const containerRef = useRef(null);
+  const containerRect = containerRef.current?.getBoundingClientRect();
+  const containerWidth = containerRect?.width || 1;
+  const containerHeight = containerRect?.height || 1;
+
+  const dispatch = useDispatch();
+  const modalAttribute = useSelector((state) => state?.modalAttribute?.data, shallowEqual);
+  const freeformBlocks = useSelector((state) => state?.freeformBlocks?.data, shallowEqual);
+  const stackBlocks = useSelector((state) => state?.stackBlocks?.data, shallowEqual);
+
+  const sensors = useSensors(
+    useSensor(PointerSensor, {
+      activationConstraint: { distance: 5 },
+    }),
+  );
+
+  const handleFreeformDragEnd = (event) => {
+    const { active, delta } = event;
+    const container = containerRef.current;
+    if (!container) return;
+
+    const rect = container.getBoundingClientRect();
+    const containerWidth = rect?.width;
+    const containerHeight = rect?.height;
+    const newBlocks = _.map(freeformBlocks, (item) => {
+      const itemId = _.get(item, ["id"]);
+      const activeId = _.get(active, ["id"]);
+      if (itemId !== activeId) {
+        return item;
+      }
+
+      const x = _.get(item, ["x"]);
+      const y = _.get(item, ["y"]);
+      const width = _.get(item, ["attribute", "width"]);
+      const height = _.get(item, ["attribute", "height"]);
+
+      const newX = (x || 0) + delta?.x;
+      const newY = (y || 0) + delta?.y;
+
+      return {
+        ...item,
+        x: Math.max(width, Math.min(newX, containerWidth)),
+        y: Math.max(height, Math.min(newY, containerHeight)),
+      };
+    });
+
+    dispatch(setFreeformBlocks(newBlocks));
+  };
+
+  const handleFreeformDragStart = () => {
+    document.body.classList.add("dragging");
+  };
+
+  const handleFreeformDragCancel = () => {
+    document.body.classList.remove("dragging");
+  };
+
+  return (
+    <Layout>
+      <ContainerEditorMenu />
+      <Container ref={containerRef} id="editor-container">
+        <DndContext
+          sensors={sensors}
+          collisionDetection={closestCenter}
+          onDragStart={handleFreeformDragStart}
+          onDragEnd={handleFreeformDragEnd}
+          onDragCancel={handleFreeformDragCancel}
+        >
+          {_.map(freeformBlocks, (item) => {
+            const id = _.get(item, ["id"]);
+            return (
+              <ContainerFreeformBlocks
+                key={id}
+                $item={item}
+                $containerWidth={containerWidth}
+                $containerHeight={containerHeight}
+              />
+            );
+          })}
+        </DndContext>
+      </Container>
+
+      <Modal>{_.get(modalAttribute, ["type"]) === "import-freeform-block" && <ImportFreeformBlock />}</Modal>
+    </Layout>
+  );
+};
